0005331f75d5762c2af9c483d541c423b82a02bf
geojwh <geojwh@gmail.com>
2016-07-29 13:19:59 -0500

    simplify do-stuff.js

--- do-stuff.js
+++ do-stuff.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
   $("h1").addClass("animated bounceInDown");
-  $("h3").addClass("animated fadeIn");
-  $("h4").addClass("animated fadeIn");
-  $("p").addClass("animated fadeIn");
-  $("ul").addClass("animated fadeIn");
-  $("img").addClass("animated fadeIn");
+  $("body").addClass("animated fadeIn");
   $("#sign").addClass("animated bounceInLeft");
   
   $('.toggle').click(function (event) {
